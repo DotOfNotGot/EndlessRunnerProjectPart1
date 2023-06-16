b7e5adce08bc4463c0885f227d69f03a02f6770f
Max Ekberg <37877493+DotOfNotGot@users.noreply.github.com>
2023-06-16 15:25:16 +0200

    INITIAL COMMIT BAYBEEEE
    oops no repo

---
+++ Source/EndlessRunnerProj/EndlessRunnerProj.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EndlessRunnerProj.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EndlessRunnerProj, "EndlessRunnerProj" );
+ 
